3fc8016869bf6c9b994dc53d6d4e59ed0919d6a5
scheidtdav <scheidtdav@users.noreply.github.com>
2020-10-06 17:12:29 +0000

    Deploying to gh-pages from  @ 4d98461a1da670966e0a07085b8dda2c9a96b4ca 🚀

---
+++ precache-manifest.0cd7133282ac74404d356413bc56ac0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe5568616410dc60cdce84e04c8a4b6",
+    "url": "/kack-und-sach-bingo/index.html"
+  },
+  {
+    "revision": "499102f29162bbbc1bce",
+    "url": "/kack-und-sach-bingo/static/js/2.59023c2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kack-und-sach-bingo/static/js/2.59023c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bd54a2af7f8a259943",
+    "url": "/kack-und-sach-bingo/static/js/main.119ded25.chunk.js"
+  },
+  {
+    "revision": "aacff6ffa0dfe98ceed5",
+    "url": "/kack-und-sach-bingo/static/js/runtime-main.77d4ac22.js"
+  },
+  {
+    "revision": "c31d61134025304426f471708f969a81",
+    "url": "/kack-und-sach-bingo/static/media/kacki.c31d6113.png"
+  }
+]);
